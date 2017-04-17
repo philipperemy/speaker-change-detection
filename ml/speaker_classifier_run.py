258b2dd34b82a5afc05953644bacbf68693c221c
philipperemy <premy@reactive.co.jp>
2017-04-17 18:14:50 +0900

    to v2

--- ml/speaker_classifier_run.py
+++ ml/speaker_classifier_run.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath('..'))
+
 from helpers.speakers_to_categorical import SpeakersToCategorical
 from ml.data_generation import generate_data
 from ml.speaker_classifier import *
