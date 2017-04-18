fix normalisation bug

ml/metrics_run.py
@@ -1,6 +1,11 @@
+import os
+import pickle
+import sys
 from glob import glob
 
 from natsort import natsorted
+
+sys.path.append(os.path.abspath('..'))
 
 from ml.conv_data_generation import generate_conv
 from ml.mfcc_data_generation import *
@@ -33,7 +38,7 @@ def process_conv(conv, t, sr, model):
         audio_slice_is_transition = is_transition(i, j, mix)
         is_transition_list.append(audio_slice_is_transition)
         mfcc_features.append(feat)
-        log_likelihoods.append(model.predict(feat))
+        log_likelihoods.append(predict(model, feat, log=True))
 
     print(np.array([inference_model(model, v) for v in mfcc_features]))
     print(np.where(np.array(is_transition_list, dtype=int))[0])
@@ -52,16 +57,25 @@ def process_conv(conv, t, sr, model):
 def find_optimal_threshold():
     # all_t = [0.5, 1.0, 2.0]
     # t = all_t[0]
-
     checkpoints = natsorted(glob('checkpoints/*.h5'))
     if len(checkpoints) == 0:
-        data = generate_data(max_count_per_class=10)
+        print('No checkpoints found.')
+        data_folder = 'data.pkl'
+        if not os.path.exists(data_folder):
+            print('Data does not exist. Generating it now.')
+            data = generate_data(max_count_per_class=1000)
+            pickle.dump(data, open(data_folder, 'wb'))
+        else:
+            print('Data found.')
+            data = pickle.load(open(data_folder, 'rb'))
+
         kx_train, ky_train, kx_test, ky_test, categorical_speakers = data_to_keras(data)
         m = get_model()
         build_model(m)
         fit_model(m, kx_train, ky_train, kx_test, ky_test, max_epochs=200)
         print(categorical_speakers.get_speaker_from_index(inference_model(m, kx_train[0:100])))
     else:
+        print('Checkpoints found.')
         from keras.models import load_model
         m = load_model(checkpoints[-1])
     train, test, sr = generate_conv()

ml/speaker_classifier.py
@@ -4,6 +4,8 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
 
+from ml.mfcc_data_generation import normalize
+
 
 def build_model(m):
     m.compile(loss='categorical_crossentropy',
@@ -25,9 +27,19 @@ def fit_model(m, kx_train, ky_train, kx_test, ky_test, max_epochs=1000):
 
 
 def inference_model(m, input_list):
-    probabilities = m.predict(input_list)
-    k_star = np.argmax(np.sum(np.log(probabilities), axis=0))
+    log_probabilities = predict(m, input_list, log=True)
+    k_star = np.argmax(np.sum(log_probabilities, axis=0))
     return k_star
+
+
+def predict(m, input_list, log=False):
+    mean = np.mean([np.mean(t) for t in input_list])
+    std = np.mean([np.std(t) for t in input_list])
+    input_list = normalize(input_list, mean, std)
+    probabilities = m.predict(input_list)
+    if log:
+        probabilities = np.log(probabilities)
+    return probabilities
 
 
 # Dropout