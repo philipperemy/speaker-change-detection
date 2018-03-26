0e75bf18cd91344cda2d5ed595d3ffe17c364f36
philipperemy <premy@reactive.co.jp>
2018-03-26 16:56:30 +0900

    getting there

--- ml/2_train_classifier.py
+++ ml/2_train_classifier.py
@@ -2,7 +2,6 @@ import os
 import pickle
 
 import numpy as np
-from tqdm import tqdm
 
 from constants import c
 from helpers.speakers_to_categorical import SpeakersToCategorical
@@ -13,7 +12,7 @@ def data_to_keras(data):
     categorical_speakers = SpeakersToCategorical(data)
     kx_train, ky_train, kx_test, ky_test = [], [], [], []
     ky_test = []
-    for speaker_id in tqdm(categorical_speakers.get_speaker_ids(), desc='data_to_keras'):
+    for speaker_id in categorical_speakers.get_speaker_ids():
         d = data[speaker_id]
         y = categorical_speakers.get_one_hot_vector(d['speaker_id'])
         for x_train_elt in data[speaker_id]['train']:
@@ -42,9 +41,13 @@ def start_training():
     print('Loading the inputs in memory. It might take a while...')
     data = pickle.load(open(data_filename, 'rb'))
     kx_train, ky_train, kx_test, ky_test, categorical_speakers = data_to_keras(data)
+    print('Dumping info about categorical speakers for the next phase (train distance classifier..')
     pickle.dump(categorical_speakers, open('/tmp/speaker-change-detection-categorical_speakers.pkl', 'wb'))
+    print('Defining model...')
     m = get_model(num_classes=num_speakers)
+    print('Building model...')
     build_model(m)
+    print('Fitting model...')
     fit_model(m, kx_train, ky_train, kx_test, ky_test)
 
 
